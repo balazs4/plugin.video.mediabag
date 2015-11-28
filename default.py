4175c77d0429e939c2f6c82328668bc559851bb3
balazs4 <balazs4web@gmail.com>
2015-11-28 15:10:32 +0100

    add "Duna" + use channels.json

--- default.py
+++ default.py
@@ -2,15 +2,20 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
+import os
+import xbmc
 import xbmcgui
 import xbmcplugin
-
+import xbmcaddon
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-channels = json.loads('[{"name": "m1", "url": "http://109.199.57.6/1VlnWLwEJHgJZQBRIWrkUoKxL_uuvLPskLoc=/9179/index.m3u8", "icon": "http://www.mediaklikk.hu/wp-content/uploads/sites/4/2013/11/M1.png"}, {"name": "m2", "url": "http://109.199.57.6/1VlnXfQEJC7PtIe5l-a9NFNT7XqL2X22W4DE=/9180/index.m3u8", "icon": "http://www.mediaklikk.hu/wp-content/uploads/sites/4/2013/11/M2.png"}, {"name": "m4", "url": "http://109.199.57.6/1VlnXfQEJC7PtIe5l-a9NFNT7XqL2X22W4DE=/9180/index.m3u8", "icon": "http://mediaklikk.cms.mtv.hu/wp-content/uploads/sites/4/2015/06/M4_logo.png"} ]')
+addonPath = xbmcaddon.Addon().getAddonInfo("path")
+channel_file = os.path.join(addonPath,'resources/data/channels.json')
+
+channels = json.load(open(channel_file));
 
 for channel in channels:
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=channel['url'], listitem=xbmcgui.ListItem(channel['name'], iconImage=channel['icon']))		
