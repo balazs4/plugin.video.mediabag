4fa29a844eb1247685e42d746f264431dd2af183
balazs4 <balazs4web@gmail.com>
2015-11-29 17:47:11 +0100

    Elminate resources

--- default.py
+++ default.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
-import os
+import urllib2
 import xbmc
 import xbmcgui
 import xbmcplugin
@@ -12,10 +12,7 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-addonPath = xbmcaddon.Addon().getAddonInfo("path")
-channel_file = os.path.join(addonPath,'resources/data/channels.json')
-
-channels = json.load(open(channel_file));
+channels = json.load(urllib2.urlopen('http://piserver:5555/channels'));
 
 for channel in channels:
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=channel['url'], listitem=xbmcgui.ListItem(channel['name'], iconImage=channel['icon']))		
