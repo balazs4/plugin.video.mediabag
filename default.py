6c26962e257c5f18d025a0e7cb0444b9ffc190a5
balazs4 <balazs4web@gmail.com>
2015-12-06 15:55:00 +0100

    Add directories (static)

--- default.py
+++ default.py
@@ -3,18 +3,42 @@
 import sys
 import json
 import urllib2
-import xbmc
+import urllib
+import urlparse
 import xbmcgui
 import xbmcplugin
-import xbmcaddon
 
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-channels = json.load(urllib2.urlopen('http://piserver:5555/channels'));
+mediaitems = json.load(urllib2.urlopen('http://127.0.0.1:5555/media'))
 
-for channel in channels:
-	xbmcplugin.addDirectoryItem(handle=addon_handle, url=channel['url'], listitem=xbmcgui.ListItem(channel['name'], iconImage=channel['icon']))		
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+mode = args.get('mode', None)
+
+if mode is None:
+    url = build_url({'mode': 'folder', 'foldername': 'live'})
+    li = xbmcgui.ListItem('LiveTV', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    url = build_url({'mode': 'folder', 'foldername': 'ejjel-nappal-budapest'})
+    li = xbmcgui.ListItem('Ejjel-Nappal Budapest', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+elif mode[0] == 'folder':
+    foldername = args['foldername'][0]
+    for item in mediaitems:
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=item['url'], listitem=xbmcgui.ListItem(item['name'], iconImage=item['icon']), isFolder=False)		
+
+
+
 
 xbmcplugin.endOfDirectory(addon_handle)
+	
