2833df7ca6082400438960ad67dd9b83ccd36084
balazs4 <balazs4web@gmail.com>
2015-12-12 20:11:55 +0100

    dynamic diretories based on tags

--- default.py
+++ default.py
@@ -8,6 +8,10 @@ import urlparse
 import xbmcgui
 import xbmcplugin
 
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
@@ -16,21 +20,20 @@ xbmcplugin.setContent(addon_handle, 'movies')
 
 mediaitems = json.load(urllib2.urlopen('http://piserver:5555/media'))
 
-def build_url(query):
-    return base_url + '?' + urllib.urlencode(query)
+tags = list()
+
+for item in mediaitems:
+    for tag in item['tags']:
+        tags.append(tag)
+
 
 mode = args.get('mode', None)
 
 if mode is None:
-    url = build_url({'mode': 'folder', 'foldername': 'live'})
-    li = xbmcgui.ListItem('LiveTV', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
-
-    url = build_url({'mode': 'folder', 'foldername': 'ejjel-nappal-budapest'})
-    li = xbmcgui.ListItem('Ejjel-Nappal Budapest', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
+    for tag in sorted(list(set(tags))):
+        url = build_url({'mode': 'folder', 'foldername': tag})
+        li = xbmcgui.ListItem(tag, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
 elif mode[0] == 'folder':
     foldername = args['foldername'][0]
